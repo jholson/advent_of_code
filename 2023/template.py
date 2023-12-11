6a6dc00a80c272e7337b73466636f731718cf6bb
Jacob Olson <jholson@users.noreply.github.com>
2023-12-11 15:57:22 -0800

    Added template file.

---
+++ 2023/template.py
@@ -0,0 +1,27 @@
+import sys
+
+from dataclasses import dataclass
+
+def main() -> None:
+    result = part1()
+    # result = part2()
+
+    print(result)
+
+
+@dataclass()
+class Thing(object):
+    pass
+
+def parse_input():
+    pass
+
+def part1() -> int:
+    return 0
+
+def part2() -> int:
+    return 0
+
+
+if __name__ == '__main__':
+    main()
